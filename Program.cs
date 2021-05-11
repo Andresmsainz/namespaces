469c080eab7fcdafa18d584840d6f8a3e3f4782f
Andres Sainz <andresmsainz@gmail.com>
2021-05-10 23:00:11 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Namespaces!");
+
+            MyCustomNamespace.Animal animal = new MyCustomNamespace.Animal();
+            animal.SayHi();
+        }
+    }
+}
+
+namespace MyCustomNamespace
+{
+    class Animal
+    {
+        public string animalColor;
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hi From the custom namespace");
+        }
+    }
+}
